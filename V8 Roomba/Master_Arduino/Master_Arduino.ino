91b4065574e6b50d2d700bd9b68a351bf89c4d55
Rayyan Mahmood <76085150+RayyanM2022@users.noreply.github.com>
2022-08-17 20:09:40 -0400

    Copying Ultra Sonic Class onto Master_Arduino file

--- V8 Roomba/Master_Arduino/Master_Arduino.ino
+++ V8 Roomba/Master_Arduino/Master_Arduino.ino
@@ -74,6 +74,53 @@ class Driver {
     }
 };
 
+class Ultrasonic {
+  private:
+    uint8_t trigPin_r;
+    uint8_t trigPin_l
+    uint8_t echoPin_r;
+    uint8_t echoPin_l;
+    const float soundSpeed = 0.034;
+  public:
+    float distance_r, distance_l;
+  
+    Ultrasonic(uint8_t trigPin_r, uint8_t echoPin_r, uint8_t echoPin_r, uint8_t echoPin_l) {
+      this->trigPin_r = trigPin_r;
+      this->trigPin_l = trigPin_l;
+      this->echoPin_r = echoPin_r;
+      this->echoPin_l = echoPin_l;
+
+      pinMode(trigPin_r, OUTPUT);
+      pinMode(trigPin_l, OUTPUT);
+      pinMode(echoPin_r, INPUT);
+      pinMode(echoPin_l, INPUT);
+    }
+
+    void measure() {
+      digitalWrite(trigPin_l, LOW);
+      delayMicroseconds(2);
+      digitalWrite(trigPin_l, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin_l, LOW);
+      
+      this->distance_r = pulseIn(echoPin_l, HIGH) * soundSpeed / 2;
+      
+      digitalWrite(trigPin_r, LOW);
+      delayMicroseconds(2);
+      digitalWrite(trigPin_r, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin_r, LOW);
+      this->distance_l = pulseIn(echoPin_r, HIGH) * soundSpeed / 2;
+
+      displayDist();
+    }
+
+    void displayDist() {
+      Serial.println("#dr" + String(distance_r) + "#");
+      Serial.println("#dl" + String(distance_l) + "#");
+    }
+};
+
 uint8_t masterAddress = 0x00;
 
 RF24 radio(9, 8);
